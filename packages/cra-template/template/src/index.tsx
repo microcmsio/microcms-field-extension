9e89fcf8f75d3751a1187e6e15b7a6acf9b5043a
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-05-09 19:35:12 +0900

    Use typescript

---
+++ packages/cra-template/template/src/index.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import * as ReactDOMClient from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = ReactDOMClient.createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
