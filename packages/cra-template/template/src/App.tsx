0259454d9a124d5df17e17251e7faacb6c22f4d0
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-05-10 16:33:47 +0900

    Handle user field

--- packages/cra-template/template/src/App.tsx
+++ packages/cra-template/template/src/App.tsx
@@ -5,11 +5,11 @@ import { ChangeEvent } from "react";
 const origin = process.env.REACT_APP_MICROCMS_ORIGIN || "https://example.microcms.io";
 
 export default function App() {
-  const [color, setColor] = useMicroCMSIframe("#00ff00", { origin });
+  const { data, setMessage } = useMicroCMSIframe("#00ff00", { origin });
 
   const onChangeColor = (e: ChangeEvent<HTMLInputElement>) => {
-    setColor({ id: "color", data: e.target.value });
+    setMessage({ id: "color", data: e.target.value });
   };
 
-  return <input type="color" value={color as string} onChange={onChangeColor} />;
+  return <input type="color" value={data as string} onChange={onChangeColor} />;
 }
--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,13 +1,17 @@
 import { useCallback, useEffect, useState } from "react";
 import * as iframe from "microcms-iframe-api";
 
-type UseMicroCMSIframe = <T>(
-  initialState: T,
-  option: iframe.SetupOption
-) => [unknown, (message: iframe.Message<T>) => void];
+type UseMicroCMSIframe = <T>(initialState: T, option: iframe.SetupOption) => UserMicroCMSIframeResult<T>;
+
+type UserMicroCMSIframeResult<T> = {
+  data: unknown;
+  setMessage: (message: iframe.Message<T>) => void;
+  user: iframe.User;
+};
 
 export const useMicroCMSIframe: UseMicroCMSIframe = <T>(initialState: T, option: iframe.SetupOption) => {
   const [id, setId] = useState<string>("");
+  const [user, setUser] = useState<iframe.User>({ email: "" });
   const [data, setData] = useState<unknown>(initialState);
 
   useEffect(() => {
@@ -16,6 +20,7 @@ export const useMicroCMSIframe: UseMicroCMSIframe = <T>(initialState: T, option:
       onDefaultData(data) {
         setId(data.data.id);
         setData(data.data.message.data);
+        setUser(data.data.user);
         if (option?.onDefaultData) {
           option.onDefaultData(data);
         }
@@ -24,7 +29,7 @@ export const useMicroCMSIframe: UseMicroCMSIframe = <T>(initialState: T, option:
     return detach;
   });
 
-  const set = useCallback(
+  const setMessage = useCallback(
     (message: iframe.Message<T>) => {
       setData(message.data);
       iframe.set({ id, message }, option.origin);
@@ -32,5 +37,5 @@ export const useMicroCMSIframe: UseMicroCMSIframe = <T>(initialState: T, option:
     [id]
   );
 
-  return [data, set];
+  return { data, setMessage, user };
 };
