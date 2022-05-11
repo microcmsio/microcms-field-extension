fd871572848d4ecf4e7fced40242d193abec0ecd
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-05-11 18:37:10 +0900

    npx create-next-app google-cloud-storage --example https://github.com/microcmsio/microcms-iframe-sdk/tree/main/examples/nextjs

---
+++ examples/google-cloud-storage/pages/index.tsx
@@ -0,0 +1,19 @@
+import { useMicroCMSIframe } from "microcms-iframe-react";
+import type { NextPage } from "next";
+import { ChangeEvent } from "react";
+
+// CHANGEME
+const origin =
+  process.env.NEXT_PUBLIC_MICROCMS_ORIGIN || "https://example.microcms.io";
+
+const Home: NextPage = () => {
+  const { data, setMessage } = useMicroCMSIframe("", { origin });
+
+  const onChangeColor = (e: ChangeEvent<HTMLInputElement>) => {
+    setMessage({ data: e.target.value });
+  };
+
+  return <input type="color" value={String(data)} onChange={onChangeColor} />;
+};
+
+export default Home;
