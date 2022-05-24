d09af42667b5d6a6c984ad273511d648f4fdf676
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-05-24 11:11:30 +0900

    Fix type

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -3,19 +3,20 @@ import {
   setupFieldExtension,
   sendFieldExtensionMessage,
   SetupOption,
+  GetDefaultDataMessage,
   Message,
   User,
 } from "microcms-field-extension-api";
 
-type UseMicroCMSIframe = <T>(initialState: T, option: SetupOption) => UserMicroCMSIframeResult<T>;
+type UseFieldExtension = <T>(initialState: T, option: SetupOption) => UseFieldExtensionReturnValue<T>;
 
-type UserMicroCMSIframeResult<T> = {
+type UseFieldExtensionReturnValue<T> = {
   data: unknown;
   sendMessage: (message: Message<T>) => void;
   user: User;
 };
 
-export const useFieldExtension: UseMicroCMSIframe = <T>(initialState: T, option: SetupOption) => {
+export const useFieldExtension: UseFieldExtension = <T>(initialState: T, option: SetupOption) => {
   const [id, setId] = useState<string>("");
   const [user, setUser] = useState<User>({ email: "" });
   const [data, setData] = useState<unknown>(initialState);
@@ -23,9 +24,11 @@ export const useFieldExtension: UseMicroCMSIframe = <T>(initialState: T, option:
   useEffect(() => {
     const detach = setupFieldExtension({
       ...option,
-      onDefaultData(data) {
+      onDefaultData(data: GetDefaultDataMessage) {
         setId(data.data.id);
-        setData(data.data.message.data);
+        if (data.data.message) {
+          setData(data.data.message.data);
+        }
         setUser(data.data.user);
         if (option?.onDefaultData) {
           option.onDefaultData(data);
