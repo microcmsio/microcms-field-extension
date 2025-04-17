3c7413bb349b0999a6718430faec71efc96df924
nakanohiroko <piroconbu@gmail.com>
2025-04-17 16:41:12 +0900

    [fix] Content-Security-Policyを設定してみる

--- examples/address-from-zipcode/next.config.js
+++ examples/address-from-zipcode/next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   devIndicators: false,
   output: 'export',
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors *',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
