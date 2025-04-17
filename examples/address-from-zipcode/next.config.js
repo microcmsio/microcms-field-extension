13a5570499308fc393a06751dde155504b4e2981
nakanohiroko <piroconbu@gmail.com>
2025-04-17 16:50:05 +0900

    [fix] output: 'export'を削除してみる

--- examples/address-from-zipcode/next.config.js
+++ examples/address-from-zipcode/next.config.js
@@ -2,21 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   devIndicators: false,
-  output: 'export',
-  trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: 'frame-ancestors *',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
